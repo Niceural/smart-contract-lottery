faf8eb25bcb6a8d3523e88bf032f07ed184466f5
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-04-17 21:58:14 +0200

    until test

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -14,6 +14,33 @@ def deploy_lottery():
     )
     print("Deployed lottery!")
 
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("The lottery is started")
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery")
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_transaction = lottery.endLottery({"from": account})
+    ending_transaction.wait(1)
+    time.sleep(60)
+    print(f"{lottery.recentWinner()} is the new winner")
 
 def main():
     deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
